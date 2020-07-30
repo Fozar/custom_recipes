332c13bbb245086e32b3b66c09f7baec9349af78
Fozar <fozar97@mail.ru>
2020-07-30 23:07:29 +0700

    Небольшой фикс

--- handlers/login.py
+++ handlers/login.py
@@ -3,6 +3,7 @@ from aiohttp_cors import CorsViewMixin
 from aiohttp_security import remember
 
 from db_auth import check_credentials
+from models import User
 
 
 class Login(web.View, CorsViewMixin):
@@ -12,5 +13,6 @@ class Login(web.View, CorsViewMixin):
             raise web.HTTPUnauthorized
 
         response = web.HTTPOk
-        await remember(self.request, response, json["login"])
+        user = await User.get(login=json["login"]).only("id")
+        await remember(self.request, response, str(user.id))
         raise response
