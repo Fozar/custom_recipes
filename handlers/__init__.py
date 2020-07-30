3913d8e8f48455382f7f6155d92a0f06baa215e6
Fozar <fozar97@mail.ru>
2020-07-30 23:17:31 +0700

    Пользователь может получить профиль любого другого пользователя

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,11 +1,12 @@
 from handlers.login import Login
 from handlers.logout import Logout
-from handlers.users import Users, UsersMe
+from handlers.users import Users, UsersMe, UsersID
 
 
 async def init_handlers(app):
     cors = app["cors"]
     cors.add(app.router.add_route("*", "/users", Users))
     cors.add(app.router.add_route("*", "/users/@me", UsersMe))
+    cors.add(app.router.add_route("*", r"/users/{id}", UsersID))
     cors.add(app.router.add_route("*", "/login", Login))
     cors.add(app.router.add_route("*", "/logout", Logout))
--- handlers/users.py
+++ handlers/users.py
@@ -3,7 +3,7 @@ from aiohttp_cors import CorsViewMixin
 from aiohttp_security import remember, check_authorized, permits
 from passlib.apps import custom_app_context as pwd_context
 
-from models import User as UserModel
+from models import User
 
 
 class Users(web.View, CorsViewMixin):
@@ -11,13 +11,13 @@ class Users(web.View, CorsViewMixin):
         """Создает нового пользователя"""
         json = await self.request.json()
         try:
-            if await UserModel.exists(login=json["login"]):
+            if await User.exists(login=json["login"]):
                 raise web.HTTPBadRequest  # Пользователь с таким именем уже существует
         except KeyError:
             raise web.HTTPBadRequest
 
         json["password_hash"] = pwd_context.hash(json.pop("password"))
-        user = await UserModel.create(**json)
+        user = await User.create(**json)
         response = web.HTTPCreated(
             headers={"Location": str(self.request.url / str(user.id))}
         )
@@ -25,15 +25,32 @@ class Users(web.View, CorsViewMixin):
         raise response
 
 
-class UsersMe(web.View, CorsViewMixin):
+class UsersID(web.View, CorsViewMixin):
+    @staticmethod
+    async def get_user_profile(user_id: int):
+        user = await User.get(pk=user_id)
+        await user.fetch_related("recipes")
+        return {
+            "id": user_id,
+            "login": user.login,
+            "status": "active",
+            "recipe_count": len(user.recipes),
+        }
+
+    async def get(self):
+        """Возвращает профиль пользователя"""
+        if not await permits(self.request, "is_active"):
+            raise web.HTTPForbidden
+
+        user_id = int(self.request.match_info["id"])
+        return web.json_response(await self.get_user_profile(user_id))
+
+
+class UsersMe(UsersID):  # Наследует от UsersID
     async def get(self):
         """Возвращает профиль авторизированного пользователя"""
         user_id = int(await check_authorized(self.request))
         if not await permits(self.request, "is_active"):
             raise web.HTTPForbidden
 
-        user = await UserModel.get(pk=user_id)
-        await user.fetch_related("recipes")
-        return web.json_response(
-            {"id": user_id, "login": user.login, "status": "active", "recipe_count": len(user.recipes)}
-        )
+        return web.json_response(await self.get_user_profile(user_id))
