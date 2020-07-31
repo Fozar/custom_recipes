9897eb2b47b854a03848b32bc49a0d1d439e2797
Fozar <fozar97@mail.ru>
2020-07-31 15:06:30 +0700

    В конфиге можно указать суперпользователя с правами администратора. Он будет создан автоматически

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ from aiohttp import web
 from aiohttp_cors import setup as setup_cors, ResourceOptions
 from aiohttp_security import setup as setup_security, SessionIdentityPolicy
 from aiohttp_session import setup as setup_session, SimpleCookieStorage
+from passlib.apps import custom_app_context as pwd_context
 from tortoise import Tortoise
 
 from db_auth import DBAuthorizationPolicy
 from handlers import init_handlers
+from models import User
 
 try:
     import uvloop
@@ -23,6 +25,18 @@ async def init_db(app):
         db_url=app["config"]["db_url"], modules={"models": ["models"]},
     )
     await Tortoise.generate_schemas()
+
+
+async def init_superuser(app):
+    su_credentials = app["config"]["superuser"]
+    if await User.exists(login=su_credentials["login"]):
+        return
+
+    await User.create(
+        login=su_credentials["login"],
+        password_hash=pwd_context.hash(su_credentials["password"]),
+        is_admin=True,
+    )
 
 
 def main():
@@ -43,6 +57,7 @@ def main():
     setup_security(app, SessionIdentityPolicy(), DBAuthorizationPolicy())
     app.on_startup.append(init_db)
     app.on_startup.append(init_handlers)
+    app.on_startup.append(init_superuser)
     web.run_app(app, host="localhost", port=5000)
 
 
